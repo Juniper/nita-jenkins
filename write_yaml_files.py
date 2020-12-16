253c62c5b900664ed73ff85d88aea3c1ab0377a6
aburston <aburston@juniper.net>
2020-12-16 13:38:47 -0800

    upgraded to python3

--- write_yaml_files.py
+++ write_yaml_files.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # ********************************************************
 #
 # Project: nita-jenkins
@@ -21,26 +21,26 @@ import os
 import stat
 
 try:
-	
-	with open('data.json') as data_file:
-		data = json.load(data_file)
-	SafeDumper.add_representer(
-	    type(None),
-	    lambda dumper, value: dumper.represent_scalar(u'tag:yaml.org,2002:null', '')
-	)
+        
+        with open('data.json') as data_file:
+                data = json.load(data_file)
+        SafeDumper.add_representer(
+            type(None),
+            lambda dumper, value: dumper.represent_scalar(u'tag:yaml.org,2002:null', '')
+        )
 
-	for filename,conf in data.iteritems():
-		if ("group_vars/" in filename or "host_vars/" in filename) and (".yaml" in filename or ".yml" in filename):
-			try:
-				yaml_content = yaml.safe_dump(conf,default_flow_style=False,explicit_start = True)
-				with open(filename, 'w') as outfile:
-   					outfile.write(yaml_content)
-					os.chmod(filename,0775)
-			except:
-				print "Yaml is not generated"
-		else:
-			print "Inavalid File Name Found =====> "+filename
+        for filename,conf in data.iteritems():
+                if ("group_vars/" in filename or "host_vars/" in filename) and (".yaml" in filename or ".yml" in filename):
+                        try:
+                                yaml_content = yaml.safe_dump(conf,default_flow_style=False,explicit_start = True)
+                                with open(filename, 'w') as outfile:
+                                        outfile.write(yaml_content)
+                                        os.chmod(filename,"0775")
+                        except:
+                                print("Yaml is not generated")
+                else:
+                        print("Inavalid File Name Found =====> "+filename)
 except: 
-	print "************** No configuration data is received **************************"
-	pass
+        print("************** No configuration data is received **************************")
+        pass
 
